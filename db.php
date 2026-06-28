7d8a4545e678f7396ccc7b9f62166b086a1084b8
Allan Kiptanui <akiptanui17@gmail.com>
2026-06-28 23:12:20 +0300

    host = "sql306.infinityfree.com "; $user = "if0_42291827"; $password = "nT3QttRuyZ"; $database = "if0_42291827_csdistsys ";

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "csdistsys";
+$host = "sql306.infinityfree.com ";
+$user = "if0_42291827";
+$password = "nT3QttRuyZ";
+$database = "if0_42291827_csdistsys ";
 
 $conn = new mysqli($host, $user, $password, $database);
 
