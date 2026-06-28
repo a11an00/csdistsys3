29eef59f078f9a649d54c34193b829eb0b6149c3
Allan Kiptanui <akiptanui17@gmail.com>
2026-06-28 22:33:30 +0300

    Initial commit

---
+++ insert.php
@@ -0,0 +1,32 @@
+<?php
+
+include("db.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $contactno = $_POST['contactno'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+
+    $sql = "INSERT INTO users
+            (fname, lname, contactno, email, address)
+            VALUES
+            ('$fname','$lname','$contactno','$email','$address')";
+
+    if ($conn->query($sql) === TRUE) {
+
+        echo "<h2>Data inserted successfully!</h2>";
+
+        echo "<a href='index.html'>Back</a>";
+
+    } else {
+
+        echo "Error: " . $conn->error;
+
+    }
+
+    $conn->close();
+}
+?>
